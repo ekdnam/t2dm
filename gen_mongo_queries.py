14512898b144ab23a54811dbf05b4038ca152631
ekdnam <adeetya.m@gmail.com>
2020-12-22 14:06:49 +0530

    push data to mongo. helper functions and helper json file added

--- gen_mongo_queries.py
+++ gen_mongo_queries.py
@@ -3,8 +3,7 @@ import os
 import pickle
 
 __MEDICAL_PARAMS__ = ["insulin", "mortality", "hazard_ratio"]
-__FILENAME__ = "insulin_mortality_hazard_ratio.xlsx"
-
+__FILENAME__ = "insulin_mortality_hazards_ratio.xlsx"
 __FILEPATH__ = f"data/sorted_excels/{__FILENAME__}"
 
 df = pd.read_excel(__FILEPATH__)
@@ -19,13 +18,6 @@ args = ["is_downloaded", "cannot_be_downloaded"]
 queries = []
 
 for pmid, title in zip(pmids, titles):
-    query = f'db.imhr.insert({{pmid: {pmid}, title: "{title}", insulin: 1, mortality: 1, hazard_ratio: 1, is_downloaded: 0, cannot_be_downloaded: 0}})'
+    query = f'db.allData.insert({{pmid: {pmid}, title: "{title}", insulin: 1, mortality: 1, hazards_ratio: 1, is_downloaded: 0, cannot_be_downloaded: 0}})'
     queries.append(query)
     print(query)
-
-# with open('scratch/insulin_mortality_hazard_ratio.txt', 'w') as f:
-#     for query in queries:
-#         f.write("%s\n" % str(query))
-
-# with open('scratch/insulin_mortality_hazard_ratio.txt', 'wb') as fp:
-#     pickle.dump(queries, fp)
---
+++ utils/helper.py
@@ -0,0 +1,11 @@
+import argparse
+import os
+import sys
+import time
+
+import selenium
+from selenium import webdriver
+
+__CHROMEDRIVERPATH__ = r"C:/chromedriver/chromedriver.exe"
+__DEFAULTPMID__ = 30694322
+__SLEEPTIME__ = 50
